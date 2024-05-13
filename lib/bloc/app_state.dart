4dd40399d0629f878faa07d56cddc3bc7480c997
sssloba <slobodan.stojakov@gmail.com>
2024-05-13 20:50:28 +0200

    app-bloc-states

---
+++ lib/bloc/app_state.dart
@@ -0,0 +1,59 @@
+import 'dart:ui';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:flutter_bloc_vandad/auth/auth_error.dart';
+
+@immutable
+abstract class AppState {
+  final bool isLoading;
+  final AuthError? authError;
+
+  const AppState({
+    required this.isLoading,
+    required this.authError,
+  });
+}
+
+@immutable
+class AppStateLoggedIn extends AppState {
+  final User user;
+  final Iterable<Reference> images;
+  const AppStateLoggedIn({
+    required this.user,
+    required this.images,
+    required super.isLoading,
+    required super.authError,
+  });
+
+  @override
+  bool operator ==(other) {
+    final otherClass = other;
+    if (otherClass is AppStateLoggedIn) {
+      return isLoading == otherClass.isLoading &&
+          user.uid == otherClass.user.uid &&
+          images.length == otherClass.images.length;
+    } else {
+      return false;
+    }
+  }
+
+  @override
+  int get hashCode => Object.hash(user.uid, images);
+
+  @override
+  String toString() => 'AppStateLoggedIn, images.length = ${images.length}';
+}
+
+@immutable
+class AppStateLoggedOut extends AppState {
+  const AppStateLoggedOut({
+    required super.isLoading,
+    required super.authError,
+  });
+
+  @override
+  String toString() =>
+      'AppStateLoggedOut, isLoading = $isLoading, authError = $authError';
+}
