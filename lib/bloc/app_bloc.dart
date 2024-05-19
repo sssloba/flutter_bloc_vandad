b939fc7658a14ac9a048274fac5cda470a68c60e
sssloba <slobodan.stojakov@gmail.com>
2024-05-19 23:17:49 +0200

    get-images-from-firestore-method

--- lib/bloc/app_bloc.dart
+++ lib/bloc/app_bloc.dart
@@ -1,10 +1,19 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_vandad/bloc/app_event.dart';
 import 'package:flutter_bloc_vandad/bloc/app_state.dart';
 
 class AppBloc extends Bloc<AppEvent, AppState> {
   AppBloc()
-      : super(const AppStateLoggedOut(
-          isLoading: false,
-        ));
+      : super(
+          const AppStateLoggedOut(
+            isLoading: false,
+          ),
+        );
+
+  Future<Iterable<Reference>> _getImages(String userId) =>
+      FirebaseStorage.instance
+          .ref(userId)
+          .list()
+          .then((listresault) => listresault.items);
 }
