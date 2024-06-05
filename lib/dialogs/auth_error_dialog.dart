09f54bd4a67bb9e6e34d6dd361b0724e3bf4a079
sssloba <slobodan.stojakov@gmail.com>
2024-06-05 18:51:48 +0200

    auth-error-dialog

---
+++ lib/dialogs/auth_error_dialog.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart' show BuildContext;
+import 'package:flutter_bloc_vandad/auth/auth_error.dart';
+import 'package:flutter_bloc_vandad/dialogs/generic_dialog.dart';
+
+Future<void> showAuthErrorDialog(BuildContext context, AuthError authError) {
+  return showGenericDialog<void>(
+    context: context,
+    title: authError.dialogTitle,
+    content: authError.dialogText,
+    optionsBuilder: () => {
+      'OK': true,
+    },
+  );
+}
