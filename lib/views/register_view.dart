0e1256ca80227abc422b0838ae4d4e2aa1fd9940
sssloba <slobodan.stojakov@gmail.com>
2024-06-07 14:31:16 +0200

    register-view

---
+++ lib/views/register_view.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_vandad/bloc/app_bloc.dart';
+import 'package:flutter_bloc_vandad/bloc/app_event.dart';
+import 'package:flutter_bloc_vandad/extensions/if_debugging.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class RegisterView extends HookWidget {
+  const RegisterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final emailController =
+        useTextEditingController(text: 'ssim.dev.test@gmail.com'.ifDebbuging);
+    final passwordController =
+        useTextEditingController(text: 'Test1234'.ifDebbuging);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Register',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                hintText: 'Enter your email here ...',
+              ),
+              keyboardType: TextInputType.emailAddress,
+              keyboardAppearance: Brightness.dark,
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(
+                hintText: 'Enter your password here ...',
+              ),
+              keyboardAppearance: Brightness.dark,
+              obscureText: true,
+              obscuringCharacter: '*',
+            ),
+            TextButton(
+              onPressed: () {
+                final email = emailController.text;
+                final password = passwordController.text;
+                context.read<AppBloc>().add(
+                      AppEventRegister(
+                        email: email,
+                        password: password,
+                      ),
+                    );
+              },
+              child: const Text(
+                'Register',
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                context.read<AppBloc>().add(
+                      const AppEventGoToLogin(),
+                    );
+              },
+              child: const Text(
+                'Already registered? Log in here!',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
