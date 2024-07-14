4be59196a8ba676c493108aee7212e62f206a9ca
sssloba <slobodan.stojakov@gmail.com>
2024-07-14 13:57:41 +0200

    deleting-home-page-and-creating-separeted-app-file

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc_vandad/firebase_options.dart';
-import 'package:flutter_bloc_vandad/home_page.dart';
+import 'package:flutter_bloc_vandad/views/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: false,
-        appBarTheme: const AppBarTheme(centerTitle: true),
-      ),
-      home: const HomePage(),
-    );
-  }
+  runApp(
+    const App(),
+  );
 }
