7d10fb043742d7b77731d64f2edcf78e83778b34
sssloba <slobodan.stojakov@gmail.com>
2024-07-14 14:04:41 +0200

    wrapping-app-with-bloc-provider

--- lib/views/app.dart
+++ lib/views/app.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_vandad/bloc/app_bloc.dart';
+import 'package:flutter_bloc_vandad/bloc/app_event.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: false,
-        appBarTheme: const AppBarTheme(centerTitle: true),
+    return BlocProvider<AppBloc>(
+      create: (_) => AppBloc()
+        ..add(
+          const AppEventInitialize(),
+        ),
+      child: MaterialApp(
+        title: 'Photo Library',
+        theme: ThemeData(
+          useMaterial3: false,
+          appBarTheme: const AppBarTheme(
+            centerTitle: true,
+          ),
+        ),
       ),
     );
   }
